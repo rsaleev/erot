a3b732e2768d7bca0981f9431455e0c6ca1e06e1
rsaleev <rs.aleev@gmail.com>
2022-02-18 19:39:30 +0300

    + логика для модели Sanction

--- src/api/loaders/database.py
+++ src/api/loaders/database.py
@@ -1,17 +1,17 @@
-from importlib import import_module
-from typing import ChainMap, List, Type, Dict, Any, Union, Tuple
-
-from itertools import chain
-from collections import ChainMap
+from typing import List, Dict, Any
 
 import asyncio
 
 from src.api.base.objects import Attribute, Object
 
-from src.database.helpers import compare_records, record_to_dict
-from src.database.models import erot
-class DatabaseLoader:
+from src.database.helpers import compare_records, chainmap_with_unique_keys, model_to_dict
+from src.database.models.erot import Base, Updates
 
+from tortoise.models import Model
+from pypika.terms import Array
+
+
+class DatabaseLoader:
 
     def _set_attr_value(self, attribute: Attribute) -> Dict[str, Any]:
         """
@@ -24,17 +24,35 @@ class DatabaseLoader:
         Returns:
             Dict[str, Any]: значения в формате k:v для передачи в инициализацию модели
         """
+        # отфилбтровать и убрать None(NULL) значения
+        if isinstance(attribute.value,list):
+            vals = [v for v in attribute.value if v]
+            # если массив со значениями, то присвоить его как значение объекта
+            if vals:
+                attribute.value = vals
+            # если после фильтрации массив пуст, то присвоить нулевое значение
+            else:
+                attribute.value = None
         output = {attribute.database.attribute: attribute.value}
         # дополнительное значение для создания записи в БД
         if attribute.database.additional_param and attribute.database.additional_attribute:
-            output.update({
-                attribute.database.additional_attribute:
-                attribute.__getattribute__(attribute.database.additional_param)
-            })
+            additional_attribute = attribute.database.additional_attribute
+            additional_value = attribute.__getattribute__(
+                attribute.database.additional_param)
+            if isinstance(additional_value,list):
+                vals = [v for v in additional_value if v]
+                # если массив со значениями, то присвоить его как значение объекта
+                if vals:
+                    additional_value = vals
+                # если после фильтрации массив пуст, то присвоить нулевое значение
+                else:
+                    additional_value = None
+            output.update(
+            {additional_attribute: additional_value})
         return output
 
-    def _set_object_values(self, object: Object,
-                          orm: str) -> Dict[str, Any]:
+    def _get_orm_values(self,
+                        attributes: List[Attribute]) -> List[Dict[str, Any]]:
         """
         Выгрузка всех значений атрибутов сгрупированных согласно названию модели ORM 
 
@@ -45,15 +63,13 @@ class DatabaseLoader:
         Returns:
             Dict[str, Any]: словарь со значениями, где ключ это имя атрибута модели
         """
-        attrs_filtered = [
-            attr for attr in object.attributes if attr.database.orm == orm
-        ]
-        values_list = [self._set_attr_value(attr) for attr in attrs_filtered]
-        output = dict(ChainMap(*values_list))
-        return output
+        values_list = [self._set_attr_value(attr) for attr in attributes]
+        return chainmap_with_unique_keys(values_list)
 
-    async def _load_parent(self, object:Object):
+    async def _load_parent(self, object: Object) -> Base:
         """
+        _load_parent
+
         Загрузка базовой записи в БД
 
         Модель Base
@@ -61,48 +77,53 @@ class DatabaseLoader:
         Args:
             object (Object): _description_
 
+        Raises:
+            ValueError: _description_
+
         Returns:
-            _type_: _description_
+            Base: запись в БД 
         """
-        base_record, base_created = await erot.Base.get_or_create(**self._set_object_values(object, 'Base'))
-        return base_record, base_created
+        attributes = [
+            attr for attr in object.attributes if attr.database._model == Base
+        ]
+        values = self._get_orm_values(attributes)
+        if len(values) > 1:
+            raise ValueError('Количество значений превышает 1')
+        record, _ = await Base.get_or_create(**values[0])
+        return record
 
-
-    async def _load_child(self, object:Object, orm:str, parent:erot.Base):
-        child_values = self._set_object_values(object, orm)
-        child_model = next(m.database._model for m in object.attributes if m.database.orm==orm)
-        child_values.update(parent.get_fk())
-        try:
-            child_record = await child_model.get_or_none(**child_values)
-        except:
-            print(child_model)
-        else:
-            if not child_record:
-                await child_model.create(**child_values)
-            else:
-                old_values = child_record.to_dict() #type: ignore
-                if changes:=compare_records(old_values, child_values):
-                    await child_model.update_or_create(**child_values)
-                    await erot.Updates.bulk_create([
-                        erot.Updates(
-                            column=ch.column,
-                            new=ch.new,
-                            old=ch.old
-                        ) for ch in changes
+    async def _load_child(self, object: Object, model: Model, parent: Base):
+        attributes = [
+            attr for attr in object.attributes if attr.database._model == model
+        ]
+        values = self._get_orm_values(attributes)
+        for val in values:
+            val.update({'req_guid_id': parent.req_guid})
+            record = await model.get_or_none(**val)
+            if record:
+                record_values = model_to_dict(record)
+                diffs = compare_records(record_values, val)
+                if diffs:
+                    if isinstance(record, list):
+                        for rec in record:
+                            await rec.update_from_dict(val).save()
+                    else:
+                        await record.update_from_dict(val).save()
+                    await Updates.bulk_create([
+                        Updates(**diff, req_uid=parent.req_guid)
+                        for diff in diffs
                     ])
+            else:
+                await model.create(**val)
             
 
-    
-    async def load(self, object:Object):
-        tasks = []
-        parent_record, parent_created = await self._load_parent(object)
-        children = tuple(set([attr.database.orm for attr in object.attributes if not attr.database.orm == 'Base']))
-        for child in children:
-            tasks.append(self._load_child(object, child, parent_record))
-        await asyncio.gather(*tasks)
-
-
-
-
-
-    
+    async def load(self, object: Object):
+        await object.map()
+        parent = await self._load_parent(object)
+        children = tuple(
+            set([
+                attr.database._model for attr in object.attributes
+                if attr.database._model != Base
+            ]))
+        await asyncio.gather(
+            *[self._load_child(object, child, parent) for child in children])
