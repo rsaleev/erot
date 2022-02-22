78e1a4b794369abd7af79ad8fd23c66d803131ff
rsaleev <rs.aleev@gmail.com>
2022-02-22 13:56:09 +0300

    рефакторинг. без динамической подгрузки модуля

---
+++ src/api/transformers/object.py
@@ -0,0 +1,22 @@
+from typing import Type
+
+from src.api import transformers
+from src.api.base.objects import Attribute, Object
+from src.api.base.transformer import BaseTransformer
+
+
+class ObjectTransformer:
+
+    @classmethod
+    def _transform_attribute(cls, attribute: Attribute):
+        if formatting := attribute.document.format:
+            for f in formatting:
+                # получениe класса из модуля
+                Formatter: Type[BaseTransformer] = getattr(
+                    transformers, f.formatter)
+                attribute.value = Formatter.transform(f.options,
+                                                      attribute.value)
+
+    @classmethod
+    def transform(cls, object: Object):
+        [cls._transform_attribute(attr) for attr in object.attributes]
