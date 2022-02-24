e35e651f94547b3ba6e55ea6c5e16bed05ab89ac
rsaleev <rs.aleev@gmail.com>
2022-02-24 14:38:55 +0300

    рефакторинг методов загрузки и обновления

--- src/database/models/erot.py
+++ src/database/models/erot.py
@@ -20,13 +20,6 @@ import re
 class ErotModel(Model):
 
     @classmethod
-    def _get_fk(cls):
-        return [
-            name for name, field in cls._meta.fields_map.items()
-            if name in cls._meta.fk_fields
-        ]
-
-    @classmethod
     async def create_or_update(cls, **kwargs):
         """
         create_or_update
@@ -43,25 +36,21 @@ class ErotModel(Model):
             2.2.3 Создание записей в таблице Updates
             2.2.4 Поиск и обновление записи Base
         """
-        if cls._get_fk():
-            raise NotImplementedError(
-                "Метод не предназначен для родительской записи")
         tasks = []
-        record = await super().get_or_none(req_guid_id=kwargs['req_guid_id'])
+        record = await super().get_or_none(req_guid_id=kwargs['parent'].req_guid)
         if not record:
-            tasks.append(super().create(**kwargs))
+            tasks.append(super().create(**kwargs, req_guid_id=kwargs['parent'].req_guid))
         else:
             record_values = model_to_dict(record)
             if diffs := compare_records(record_values, kwargs):
                 tasks.append(
                     Updates.bulk_create([
-                        Updates(**diff, req_guid=kwargs['req_guid_id'])
+                        Updates(**diff, req_guid=kwargs['parent'].req_guid)
                         for diff in diffs
                     ]))
                 record.update_from_dict(kwargs)
                 tasks.append(record.save())
-                parent = await Base.get(req_guid=kwargs['req_guid_id'])
-                tasks.append(parent.save())
+                tasks.append(kwargs['parent'].save())
         await asyncio.gather(*tasks)
 
     class Meta:
@@ -126,7 +115,7 @@ class Control(ErotModel):
 
 
 class Compliance(ErotModel):
-    cmpl_act_type = IntField(null=False)
+    cmpl_act_type = IntField(null=True)
     cmpl_act_title = TextField(null=False)
     cmpl_act_text = TextField(null=True)
     cmpl_act_approved_org = TextField(null=False)
@@ -164,12 +153,9 @@ class Liability(ErotModel):
 class Sanction(ErotModel):
     id = IntField(pk=True)
     snct_subject = IntField(null=False)
-    snct_title = TextField(null=True)
-    snct_content = TextField(null=True)
-    snct_min = IntField(null=True)
-    snct_max = IntField(null=True)
-    snct_measure = TextField(null=True)
-    snct_comments = TextField(null=True)
+    snct_title = TextArrayField(null=True)
+    snct_content = TextArrayField(null=True)
+    snct_comments = TextArrayField(null=True)
 
     req_guid = ForeignKeyField('erot.Base',
                                to_field='req_guid',
@@ -178,78 +164,37 @@ class Sanction(ErotModel):
     @classmethod
     async def create_or_update(cls, **kwargs):
         """
-        Sanction.create(cls, **kwargs)
+        create_or_update
 
-        перезагрузка метода для создания одиночных записей, если в значениях указаны несколько субъектов
+        Измененнный метод для обработки массива значений (k:v) на входе или единичного значения (k:v)
+
+        1. Поиск значений в БД
+
+        2.1 Если запись не существует -> создание новой записи
+
+        2.2 Если запись существует:
+            2.2.1 Сверка значений в БД и новых значений 
+            2.2.2 Обновление записи в БД
+            2.2.3 Создание записей в таблице Updates
+            2.2.4 Поиск и обновление записи Base
         """
         tasks = []
-        # разбор массива значений на единичные словари
-        for title in kwargs['snct_title']:
-            # создание пустого словаря
-            single_data = {}
-            # назначение типа субъекта
-            single_data['snct_subject'] = kwargs['snct_subject']
-            # назначение типа санкциии
-            single_data['snct_title'] = title
-            record = await cls.get_or_none(req_guid_id=kwargs['req_guid_id'],
-                                           snct_title=title,
-                                           snct_subject=kwargs['snct_subject'])
-            # мэппинг соотвутствующей санкции:
-            # Административный штраф: от 1000 до 5000 (в рублях)
-            content = next(
-                (c.replace(title, '').replace(':', '').strip()
-                 for c in kwargs['snct_content'] if c.rfind(title) > -1), None)
-            single_data['snct_content'] = content
-            if kwargs['snct_comments']:
-                single_data['snct_comments'] = next(
-                    (c.replace(title, '').replace(':', '').strip()
-                    for c in kwargs['snct_comments'] if c.rfind(title) > -1),
-                    None)
-            # парсинг санкции с разбором на минимальное значение/максимальное и тип калькуляции
-            if content:
-                content_measures = re.match(r"^(?<min>от\s*(?<min_val>\d+))|.*(?<max>до\s*(?<max_val>\d+))|.*(?<measure>\(...*\))$", content)
-                if not content_measures:
-                    raise ValueError
-                single_data['snct_min'] = content_measures.groupdict('min')
-                single_data['snct_max'] = content_measures.groupdict('max')
-                single_data['snct_measure'] = content_measures.groupdict('measure')
-            single_data.update(kwargs['req_guid_id'])
-            if record:
-                if record.snct_min != single_data['snct_min']:
-                    record.update_from_dict({'snct_min':single_data['snct_min']})
-                    tasks.append(Updates.create(old=record.snct_min, new=single_data['snct_min'], column='snct_min'))
-                if record.snct_max !=  single_data['snct_max']:
-                    record.update_from_dict({'snct_max':single_data['snct_max']})
-                    tasks.append(Updates.create(old=record.snct_max, new=single_data['snct_max'], column='snct_max'))
-                if record.snct_measure !=  single_data['snct_measure']:
-                    record.update_from_dict({'snct_measure':single_data['snct_measure']})
-                    tasks.append(Updates.create(old=record.snct_measure, new=single_data['snct_measure'], column='snct_measure'))
+        record = await super().get_or_none(req_guid_id=kwargs['parent'].req_guid, snct_subject=kwargs['snct_subject'])
+        if not record:
+            tasks.append(super().create(**kwargs, req_guid_id=kwargs['parent'].req_guid))
+        else:
+            record_values = model_to_dict(record)
+            if diffs := compare_records(record_values, kwargs):
+                tasks.append(
+                    Updates.bulk_create([
+                        Updates(**diff, req_guid=kwargs['parent'].req_guid)
+                        for diff in diffs
+                    ]))
+                record.update_from_dict(kwargs)
                 tasks.append(record.save())
-            else:
-                tasks.append(super().create(**single_data))
+                tasks.append(kwargs['parent'].save())
         await asyncio.gather(*tasks)
 
-    # @classmethod
-    # async def get_or_none(cls, **kwargs):
-    #     tasks = []
-    #     if kwargs['snct_title']:
-    #         for title in kwargs['snct_title']:
-    #             single_data = {}
-    #             single_data['snct_subject'] = kwargs['snct_subject']
-    #             single_data['snct_title'] = title
-    #             single_data['snct_content'] = next(
-    #                 (c.replace(title, '').replace(':', '').strip()
-    #                  for c in kwargs['snct_content'] if c.rfind(title) > -1),
-    #                 None)
-    #             if kwargs['snct_comments']:
-    #                 single_data['snct_comments'] = next(
-    #                     (c.replace(title, '').replace(':', '').strip()
-    #                      for c in kwargs['snct_comments']
-    #                      if c.rfind(title) > -1), None)
-    #             single_data.update({'req_guid_id': kwargs['req_guid_id']})
-    #             tasks.append(super().get_or_none(**single_data))
-    #         return await asyncio.gather(*tasks)
-    #     return
 
     class Meta:
         app = "erot"
@@ -291,3 +236,6 @@ __models__ = [
     Base, Description, Compliance, Liability, Sanction, Control, Attribute,
     Updates
 ]
+
+__all__ = ['Base', 'Description', 'Compliance', 'Liability', 'Sanction', 'Control', 'Attribute',
+    'Updates']
