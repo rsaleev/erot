73612214d43b22ac578309531ff124fdce1754ed
rsaleev <rs.aleev@gmail.com>
2022-02-24 14:37:40 +0300

    транформация значений перед загузкой в БД

--- src/api/transformers/database.py
+++ src/api/transformers/database.py
@@ -1,4 +1,7 @@
+from curses.ascii import isascii
 from typing import Type, Dict, Any
+
+from attr import attr
 
 from src.api import transformers
 from src.api.base.objects import Attribute, Object
@@ -21,21 +24,4 @@ class DatabaseTransformer:
             _type_: _description_
         """
         
-        if formatting:=attribute.database.format:
-            for f in formatting:
-                # получениe класса из модуля
-                Formatter:Type[BaseTransformer] = getattr(transformers, f.formatter)
-                value = Formatter.transform(output[f.attribute])
-                if f.output:
-                    if not f.attribute in tuple(f.output.keys()):
-                        output.pop(f.attribute)
-                    for k, v in f.output.items():
-                        val = None
-                        if value:
-                            val = value[v]
-                        output.update({k: val})
-                else:
-                    output[f.attribute]
-                
-
    
