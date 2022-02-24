aa1ad08faf462ba9641df492086f2e9e8b0aafc8
rsaleev <rs.aleev@gmail.com>
2022-02-24 14:36:26 +0300

    добавлены атрибуты Mapping

--- src/api/base/schema.py
+++ src/api/base/schema.py
@@ -12,52 +12,55 @@ from pydantic import Field
 class Format(AbstractModel):
     formatter: str
     options: List[str]
-    output:Optional[Dict[str, str]]
-    attribute:str
-
-
+    output: Optional[Dict[str, str]]
+    attribute: Optional[str]
 
 
 class Document(AbstractModel):
     regex: str
     index: int
     optional: bool
-    format: Union[None, List[Format]]
+    format: Optional[List[Format]] = None
 
 
 class Mapping(AbstractModel):
+    source:str
     input: str
     link: str
     output: str
 
-    
+
 class Database(AbstractModel):
     orm: str
-    attribute: str
-    param: Dict[str, str]
-    format:Optional[List[Format]]
+    params: Dict[str, str]
+    format: Optional[List[Format]]
+
 
 class SchemaColumn(AbstractModel):
     name: str
     document: Document
     mapping: Optional[List[Mapping]]
-    database: Optional[Database]
+    database: Optional[Database] = None
 
-    
+
 class DocumentSchemaMissingColumns(AbstractModel):
 
     required: Optional[List[str]]
     optional: Optional[List[str]]
 
+
 class HeaderAttribute(AbstractModel):
     name: str
     optional: bool
+
+
 class DocumentSchemaData(AbstractModel):
 
     name: str = Field(..., alias='schema')
     header: Optional[List[HeaderAttribute]]
     columns: List[SchemaColumn]
     missing: Optional[DocumentSchemaMissingColumns]
+
 
 class DocumentSchemaResponse(AbstractModel):
 
