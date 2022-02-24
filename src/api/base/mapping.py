ca470562f08d0cf6cc17036cb0161a3f50b68573
rsaleev <rs.aleev@gmail.com>
2022-02-24 14:35:37 +0300

    клиент для получения пребразованных значений

---
+++ src/api/base/mapping.py
@@ -0,0 +1,47 @@
+import asyncio
+from abc import ABC
+from typing import Any, Union, Tuple
+import os
+
+from aiohttp import ClientSession
+from src.api.base.schema import Mapping
+
+
+class BaseMapper(ABC):
+
+    @classmethod
+    async def _get(cls, mapping:Mapping, value:Any) ->Any:
+            """
+            Получение соответствующего значения из источника
+            Args:
+                map (schema.Mapping): [description]
+                arg (str): [description]
+
+            Returns:
+                [type]: [description]
+            """
+
+            if value:
+                # загрузка URL из переменных окружения
+                try:
+                    url = os.environ[f'{mapping.source.upper()}_URL']
+                except KeyError:
+                    return
+                async with ClientSession(raise_for_status=True) as session:
+                    try:
+                        async with session.get(
+                                url=f"{url}{mapping.link}/{value}") as response:
+                                # чтение результата
+                                response_data = await response.json()
+                                # получение значения dict по ключу
+                                response_data[mapping.output]
+                    except:
+                        return 
+            return 
+
+    @classmethod
+    async def fetch(cls, mapping:Mapping, value:Any) ->Union[Tuple[Any, ...], Any]:    
+        if isinstance(value, list):
+            return await asyncio.gather(*[cls._get(mapping,val) for val in value])
+        else:
+            return await cls._get(mapping, value)
